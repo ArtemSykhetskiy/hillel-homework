565b642c52aca83cfb7e7a39c84cab39ceb3121d
ArtemSykhetskiy <2210ams@gmail.com>
2022-06-02 22:59:24 +0300

    Homework 4

---
+++ App/Http/Controllers/Admin/OrdersController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+class OrdersController{
+    public function __construct(){
+        echo __CLASS__;
+    }
+}
---
+++ App/Models/Order.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models;
+
+class Order
+{
+    public function __construct()
+    {
+        echo __CLASS__;
+    }
+}
