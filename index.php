728f5b906c69def3481376db55e5dbacba4f96dd
ArtemSykhetskiy <2210ams@gmail.com>
2022-06-13 17:24:11 +0300

    HM9

--- index.php
+++ index.php
@@ -1,25 +1,86 @@
 <?php
-    require_once __DIR__. '/classes/DBConnect.php';
-    $db = new \classes\DBConnect();
-    $isTable = $db->tableExists('user');
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['newTable'] == 1){
-        $db->createTable('user');
-        header('Location:index.php');
+interface Format
+{
+    public function setFormat(String $logger);
+}
+
+interface Deliver
+{
+    public function setDeliver(Format $deliver);
+}
+
+class FormatRaw implements Format
+{
+    public function setFormat(String $string): string{
+        return $string;
     }
+}
+class FormatWithDate implements Format
+{
+
+    public function setFormat(String $string): string{
+        return date('Y-m-d H:i:s') . $string;
+    }
+}
+class FormatWithDateAndDetails implements Format
+{
+    public function setFormat(String $string): string{
+        return date('Y-m-d H:i:s') . $string . ' - With some details';
+    }
+}
+
+class DeliverByEmail implements Deliver
+{
+    public function setDeliver($format){
+        echo "Вывод формата ({$format}) по имейл";
+    }
+}
+
+class DeliverBySms implements Deliver
+{
+    public function setDeliver($format){
+        echo "Вывод формата ({$format}) в смс";
+    }
+}
+
+class DeliverByConsole implements Deliver
+{
+    public function setDeliver($format){
+        echo "Вывод формата ({$format}) в консоль";
+    }
+}
+
+
+class Logger
+{
+    private $format;
+    private $delivery;
+
+    public function __construct(Format $format, Deliver $delivery)
+    {
+        $this->format   = $format;
+        $this->delivery = $delivery;
+    }
+
+    public function log($string)
+    {
+        $this->deliver($this->format($string));
+    }
+
+    public function format($string)
+    {
+       return $this->format->setFormat($string);
+    }
+
+    public function deliver( $format)
+    {
+        $this->delivery->setDeliver($format);
+    }
+
+}
+
+$logger = new Logger(new FormatWithDate(), new DeliverByConsole());
+$logger->log('Fixed by SOLID!');
+
 ?>
-<?php if($isTable === false):?>
-<form method="POST">
-    <button type="submit" value="1" name="newTable">Create table 'Users'</button>
-</form>
-<?php else:?>
-<p>Table 'Users' is already exists</p>
-<?php endif;?>
-
-<hr>
-<a href="index.php">Main | </a>
-<a href="addUser.php">Add User | </a>
-<a href="showAllUsers.php">Show all users</a>
-<a href="deleteUser.php">Delete user</a>
-
-<?php  $db = null; ?>
