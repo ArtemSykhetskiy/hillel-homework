816902dbbb531a3a1af7a2adf408b34bce6092da
ArtemSykhetskiy <2210ams@gmail.com>
2022-06-18 12:00:53 +0300

    HM11

--- index.php
+++ index.php
@@ -1,86 +1,79 @@
 <?php
 
-interface Format
-{
-    public function setFormat(String $logger);
-}
+abstract class TaxiType{
 
-interface Deliver
-{
-    public function setDeliver(Format $deliver);
-}
+    abstract public function createTaxi() : Type;
 
-class FormatRaw implements Format
-{
-    public function setFormat(String $string): string{
-        return $string;
-    }
-}
-class FormatWithDate implements Format
-{
+    public function taxiInfo(){
+        $taxi = $this->createTaxi();
+        $result = 'Taxi model -> ' . $taxi->taxiModel() . "<br>";
+        $result.= 'Price -> ' . $taxi->taxiPrice(). "<br>";
 
-    public function setFormat(String $string): string{
-        return date('Y-m-d H:i:s') . $string;
-    }
-}
-class FormatWithDateAndDetails implements Format
-{
-    public function setFormat(String $string): string{
-        return date('Y-m-d H:i:s') . $string . ' - With some details';
+        return $result;
     }
 }
 
-class DeliverByEmail implements Deliver
-{
-    public function setDeliver($format){
-        echo "Вывод формата ({$format}) по имейл";
-    }
-}
-
-class DeliverBySms implements Deliver
-{
-    public function setDeliver($format){
-        echo "Вывод формата ({$format}) в смс";
-    }
-}
-
-class DeliverByConsole implements Deliver
-{
-    public function setDeliver($format){
-        echo "Вывод формата ({$format}) в консоль";
-    }
-}
-
-
-class Logger
-{
-    private $format;
-    private $delivery;
-
-    public function __construct(Format $format, Deliver $delivery)
+class Econom extends TaxiType{
+    public function createTaxi(): Type
     {
-        $this->format   = $format;
-        $this->delivery = $delivery;
+        return new TaxiEconom();
     }
-
-    public function log($string)
-    {
-        $this->deliver($this->format($string));
-    }
-
-    public function format($string)
-    {
-       return $this->format->setFormat($string);
-    }
-
-    public function deliver( $format)
-    {
-        $this->delivery->setDeliver($format);
-    }
-
 }
 
-$logger = new Logger(new FormatWithDate(), new DeliverByConsole());
-$logger->log('Fixed by SOLID!');
 
-?>
+class Standart extends TaxiType{
+    public function createTaxi(): Type
+    {
+        return new TaxiStandart();
+    }
+}
+
+
+class Lux extends TaxiType{
+    public function createTaxi(): Type
+    {
+        return new TaxiLux();
+    }
+}
+
+interface Type{
+    public function taxiModel();
+    public function taxiPrice();
+}
+
+class TaxiEconom implements Type{
+    public function taxiModel() : string{
+        return 'Toyota Corolla';
+    }
+    public function taxiPrice() : int{
+        return 10;
+    }
+}
+
+class TaxiStandart implements Type{
+    public function taxiModel() : string{
+        return 'Toyota Camry';
+    }
+    public function taxiPrice() : int{
+        return 15;
+    }
+}
+
+class TaxiLux implements Type{
+    public function taxiModel() : string{
+        return 'Mersedes S class';
+    }
+    public function taxiPrice() : int{
+        return 20;
+    }
+}
+
+function clientCode(TaxiType $taxiType){
+    echo $taxiType->taxiInfo();
+}
+
+
+clientCode(new Econom());
+clientCode(new Lux());
+clientCode(new Standart());
+
