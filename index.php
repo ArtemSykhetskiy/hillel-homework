9b84f239c9077841104f1d3c94b6f921334ffb99
ArtemSykhetskiy <2210ams@gmail.com>
2022-06-19 11:01:25 +0300

    HM10

--- index.php
+++ index.php
@@ -1,73 +1,55 @@
 <?php
 
-interface ContactDetail
+abstract class PaymentMethod
 {
-    public function name($name) : void;
-    public function phone($phone) : void;
-    public function surname($surname) : void;
-    public function email($email) : void;
-    public function address($address) : void;
-}
+    abstract public function ConcretePymentMethod() : ConcreteMethod;
 
-class ContactDetails implements ContactDetail {
-    private $contactDetails;
-
-    public function __construct(){
-        $this->reset();
-    }
-
-    public function reset()
+    public function doingSomeOperations()
     {
-        $this->contactDetails = new Contact();
-    }
-
-    public function phone($phone) : void
-    {
-        $this->contactDetails->parts[] = $phone;
-    }
-
-    public function name($name) : void
-    {
-       $this->contactDetails->parts[] = $name;
-    }
-
-    public function surname($surname) : void
-    {
-        $this->contactDetails->parts[] = $surname;
-    }
-
-    public function email($email) : void
-    {
-        $this->contactDetails->parts[] = $email;
-    }
-
-    public function address($address) : void
-    {
-        $this->contactDetails->parts[] = $address;
-    }
-    public function build()
-    {
-       echo implode(', ', $this->contactDetails->parts);
-        $this->reset();
-
+        // TODO Somethings
     }
 }
 
-class Contact
+class PayPal extends PaymentMethod
 {
-    public $parts = [];
+
+    public function ConcretePymentMethod(): ConcreteMethod
+    {
+        // TODO: Implement ConcretePymentMethod() method.
+
+        return new PayPalMethod;
+    }
 }
 
+class CreditCard extends PaymentMethod
+{
 
-$contact1 = new ContactDetails();
-$newContact = $contact1->name("John");
-$newContact = $contact1->surname("Smith");
-$newContact = $contact1->phone("24235225");
-$newContact = $contact1->address("Ukraine");
+    public function ConcretePymentMethod(): ConcreteMethod
+    {
+        // TODO: Implement ConcretePymentMethod() method.
 
-echo "<br><br>";
+        return new CreditCardMethod;
+    }
+}
 
-$newContact = $contact1->name("Jack");
-$newContact = $contact1->surname("Taylor");
-$newContact = $contact1->build();
+interface ConcreteMethod
+{
+    public function operation();
+}
+
+class PayPalMethod implements ConcreteMethod
+{
+    public function operation()
+    {
+        // TODO: Implement operation() method.
+    }
+}
+
+class CreditCardMethod implements ConcreteMethod
+{
+    public function operation()
+    {
+        // TODO: Implement operation() method.
+    }
+}
 
